4fe02842ec0fdb869b8d3a404c44e2ccfa72b397
Anita Samuel <anita.samuel@web.de>
2021-01-29 18:13:43 +0000

    2.9*: The Phonebook Step4

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -1,18 +1,38 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
+
+
+const FilterSearch = (props) => {
+  const {searchAction} = props
+  return(
+    <div>
+      <h2>Phonebook</h2>
+      <form>
+        <label>filter shown with:</label>
+        <input onChange={searchAction}/>
+      </form>
+    </div>
+  )
+};
 
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-1234567'},
+    {name: 'Ada Lovelace', number: '049-12345678'}
   ]);
   const [ newName, setNewName ] = useState('');
   const [ newNumber, setNewNumber ] = useState('');
-  
+  const [ newSearch, setNewSearch ] = useState('');
+  console.log(newSearch.toLowerCase(), 'lowerCase')
   const handleNameChange = (event) => {
     setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
     setNewNumber(event.target.value);
+  };
+
+  const handleSearch = (event) => {
+    setNewSearch(event.target.value);
   }
   
   const filterName = persons.filter(person => (person.name.toLowerCase() === newName.toLowerCase()));
@@ -29,36 +49,43 @@ const App = () => {
     } 
     return setPersons(persons.concat(noteObject))
 
+  };
+
+  const displayAllNames = () => persons.map( person => <li key={person.name}> {person.name} {person.number}</li>);
+
+  const displayFilteredName = () => {
+    return persons.filter(people =>  people.name.toLowerCase().includes(newSearch.toLowerCase())).map(per => <li> {per.name} {per.number} </li>)
   }
 
-  const Persons = () => (
-          <ul>
-        {persons.map( person => 
-          <li key={person.name}> {person.name} {person.number}</li>
-        )}
+  const personsList = () => (
+      <ul>
+        {newSearch.length ? displayFilteredName(): displayAllNames()}
       </ul>
   );
-
+  
   return (
-    <div>
-      <h2>Phonebook</h2>
-      <form onSubmit={addNewName}>
-        <div>
-          <label for='personName'>name:</label>
-          <input value={newName} name='personName' onChange={handleNameChange}/>
-        </div>
-        <div>
-        <label for='phoneNumber'>number:</label>
-        <input value={newNumber} name='phoneNumber' onChange={handleNumberChange}/>
-        </div>
-        <div>
-          <button type="submit">add</button>
-          <div> {newName} </div>
-        </div>
-      </form>
-      <h2>Numbers</h2>
-      <Persons/>
-    </div>
+    <>
+      <FilterSearch searchAction={handleSearch} />
+      <div>
+        <h2>add a new</h2>
+        <form onSubmit={addNewName}>
+          <div>
+            <label htmlFor='personName'>name:</label>
+            <input value={newName} name='personName' onChange={handleNameChange}/>
+          </div>
+          <div>
+          <label htmlFor='phoneNumber'>number:</label>
+          <input value={newNumber} name='phoneNumber' onChange={handleNumberChange}/>
+          </div>
+          <div>
+            <button type="submit">add</button>
+            <div> {newName} </div>
+          </div>
+        </form>
+        <h2>Numbers</h2>
+        {personsList()}
+      </div>
+    </>
   );
 }
 
